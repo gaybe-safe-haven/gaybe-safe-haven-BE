2541f7ae52b318bf2a87d2c696e32fb5aa1512e1
Patricia Severance <patriciakseverance@gmail.com>
2023-03-29 16:09:00 -0600

    [WIP] Add logic for review creation (Maybe)

--- src/controllers/reviews_controller.ts
+++ src/controllers/reviews_controller.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+import { PrismaClient, Prisma } from '@prisma/client'
 
 export const reviewController = express.Router();
 
-reviewController.post('/', (req, res) => {
-  res.status(201).send('')
+const prisma = new PrismaClient();
+
+reviewController.post('/', async (req, res) => {
+  // res.status(201).send('')
+
+  let shelterId: any = req.query.shelterId;
+  let cleanliness: any = req.query.cleanliness;
+  let safety: any = req.query.safety;
+  let staff: any = req.query.staff;
+
+  const review = await prisma.review.create({
+      data: {
+        shelterId: shelterId,
+        cleanliness: cleanliness,
+        safety: safety,
+        staff: staff,
+      },
+    });
 });
