e4c4f877d451edf8ff238c6bb56282e4b6d30932
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-03-29 20:18:04 -0500

    add serialized endpoint for post shelter per test

--- src/controllers/shelters_controller.ts
+++ src/controllers/shelters_controller.ts
@@ -1,7 +1,18 @@
 import express from 'express';
-
+import prisma from '../db/prisma.config';
+import { Shelter } from '@prisma/client';
+import { serializeShelter } from '../serializers/shelter'
 export const shelterController = express.Router();
 
-shelterController.post('/', (req, res) => {
-  res.status(201).send('');
+shelterController.post('/', async (req, res) => {
+  try { 
+    const shelterData = req.body;
+    const shelter: Shelter = await prisma.shelter.create({ data: shelterData });
+    res.status(201).send({
+      data: serializeShelter(shelter)
+    });
+  } catch (err) {
+    res.status(500).send(err); 
+      
+  }
 })
