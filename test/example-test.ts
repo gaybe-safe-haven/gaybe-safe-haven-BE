1bb9a61275fcb61d26d55608e374fce7b3e17c44
Thomas Casady <tscasady@gmail.com>
2023-03-30 09:22:01 -0500

    Remove example tests and example comments for controller.

--- test/example-test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import * as test from '../src/example';
-
-describe('Github Actions Test', () => {
-  it('should return true if given a value', () => {
-    const example = test.testFunction('working')
-    expect(example).to.equal(true)
-  })
-})
