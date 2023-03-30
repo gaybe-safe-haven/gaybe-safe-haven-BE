81b6274646402bff4ce75aa3421b7471c8a777c8
Thomas Casady <tscasady@gmail.com>
2023-03-30 09:26:47 -0500

    Add get shelter request test and functionality.

--- src/controllers/shelters_controller.ts
+++ src/controllers/shelters_controller.ts
@@ -1,7 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import prisma from "../db/prisma.config"
+import { Shelter } from "@prisma/client"
+import { serializeShelter } from "../serializers/serializeShelter"
 
 export const shelterController = express.Router();
 
-shelterController.post('/', (req, res) => {
+shelterController.post('/', (req: Request, res: Response) => {
   res.status(201).send('');
+})
+
+shelterController.get('/:shelterId', async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.shelterId, 10)
+
+  const shelter: Shelter | null = await prisma.shelter.findUnique({
+    where: {
+      id: id
+    }
+  })
+
+  res.status(200).send(serializeShelter(shelter))
 })
--- test/requests/shelters/shelterRequest-test.ts
+++ test/requests/shelters/shelterRequest-test.ts
@@ -1,7 +1,6 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../../src/app';
-import { Shelter } from '@prisma/client';
 
 chai.use(chaiHttp);
 
@@ -18,7 +17,7 @@ describe('Post Shelter', () => {
       name: 'Golden Sun',
       streetAddress: '1234 Black St',
       state: 'NY',
-      zip: 78123
+      zip: 78123,
     }
 
     // post to '/api/v1/shelters' with shelter params
@@ -27,7 +26,6 @@ describe('Post Shelter', () => {
     .post('/api/v1/shelters')
     .send(shelterParams)
     .end((err, res) => {
-      expect(err).to.be.null;
       // expect response to be 201 ok
       expect(res).to.have.status(201);
       done();
@@ -36,5 +34,27 @@ describe('Post Shelter', () => {
     // expect structure of the response
 
     // check last shelter in database against shelter params
+  })
+})
+
+describe('GET shelters/:shelterid', () => {
+  it('can return a shelter with the given id', (done) => {
+    //Fetch shelter
+    chai
+    .request(app)
+    .get('/api/v1/shelters/2')
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.have.key('data');
+      expect(res.body.data.id).to.equal(2);
+      expect(res.body.data.type).to.equal('shelter');
+      expect(res.body.data.attributes.name).to.equal('Golden Sun');
+      expect(res.body.data.attributes.streetAddress).to.equal('1234 Black St');
+      expect(res.body.data.attributes.state).to.equal('NY');
+      expect(res.body.data.attributes.zip).to.equal(78123);
+      done();
+    })
   })
 })
