845acfa434ee2ceb367768edee31bb745bf476c6
Patricia Severance <patriciakseverance@gmail.com>
2023-04-03 13:03:08 -0600

    Refactor types to accept null for optional args

--- src/reviews/review.types.ts
+++ src/reviews/review.types.ts
@@ -1,6 +1,6 @@
 export type ReviewPost = {
   shelterId: number,
-  cleanliness?: number,
-  safety?: number,
-  staff?: number,
+  cleanliness?: number | null,
+  safety?: number | null,
+  staff?: number | null,
 }
