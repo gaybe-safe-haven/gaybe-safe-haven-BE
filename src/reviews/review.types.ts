e641e0604362e38cfa616b3d328f9cc6a491d94c
Thomas Casady <33361274+Tscasady@users.noreply.github.com>
2023-04-04 17:36:25 -0500

    Update review.types.ts

--- src/reviews/review.types.ts
+++ src/reviews/review.types.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const reviewPostValidator = z.object({
-  shelterId: z.number(),
+  shelterId: z.coerce.number(),
   cleanliness: z.coerce.number().min(0).max(10),
   safety: z.coerce.number().min(0).max(10),
   staff: z.coerce.number().min(0).max(10)
