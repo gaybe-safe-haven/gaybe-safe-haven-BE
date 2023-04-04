83552ec24d98ed94d2a5d876871a7ab02e180efa
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-04-04 17:27:26 -0500

    fix file path typo

--- src/controllers/reviews_controller.ts
+++ src/controllers/reviews_controller.ts
@@ -3,7 +3,7 @@ import prisma from '../db/prisma.config';
 import { Review } from '@prisma/client';
 import { serializeReview } from '../serializers/review';
 import { errorHandler } from '../errors/errorHandler'
-import { reviewPostValidator, ReviewPost } from '../../src/reviews/review.types';
+import { reviewPostValidator, ReviewPost } from '../reviews/review.types';
 
 export const reviewController = express.Router();
 
