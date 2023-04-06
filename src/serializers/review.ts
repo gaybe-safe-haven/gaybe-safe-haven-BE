1fff9437c42a586945bc41281f4e18ae26b8d9c6
Patricia Severance <patriciakseverance@gmail.com>
2023-04-05 18:09:05 -0600

    Remove null case handling in review.ts

--- src/serializers/review.ts
+++ src/serializers/review.ts
@@ -3,18 +3,14 @@ import { Review } from '@prisma/client'
 
 
 export function serializeReview(review: Review) {
-  if (review) {
-    return {
-      id: review.id,
-      type: 'review',
-      attributes: {
-        shelterId: review.shelterId,
-        cleanliness: review.cleanliness,
-        safety: review.safety,
-        staff: review.staff,
-      },
-    }
-  } else {
-    return { data: {} }
+  return {
+    id: review.id,
+    type: 'review',
+    attributes: {
+      shelterId: review.shelterId,
+      cleanliness: review.cleanliness,
+      safety: review.safety,
+      staff: review.staff,
+    },
   }
 }
